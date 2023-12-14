ea5e1c93ddddfc716b1e48eec2256a5b7bfcf6cb
panxiaoteng <chengdaguo127@163.com>
2023-12-14 11:07:28 +0800

    Update data 12.01-07 by pxt.

--- generate_result_py/config.py
+++ generate_result_py/config.py
@@ -3,7 +3,10 @@ class DefaultConfig(object):
     file_path_week2 = "../data/sat_result_20231110_20231116/com_sat_result/"
     file_path_week3 = "../data/sat_result_20231117_20231123/com_sat_result/"
     file_path_week4 = "../data/sat_result_20231124_20231130/com_sat_result/"
-    file_path_last_week = "../data/sat_result_20231124_20231130/"
-    pathArray = [file_path_week1, file_path_week2, file_path_week3, file_path_week4]
-    week_num = 4
-    start_date = "2023-11-24"
+    file_path_week5 = "../data/sat_result_20231201_20231207/com_sat_result/"
+    # 修改
+    file_path_last_week = "../data/sat_result_20231201_20231207/"
+    pathArray = [file_path_week1, file_path_week2, file_path_week3, file_path_week4, file_path_week5]
+    week_num = 5
+    start_date = "2023-12-01"
+    # 修改
