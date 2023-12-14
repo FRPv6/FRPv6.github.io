eba967f60ae6eea2f3193a73984f97e3fa5a74ef
panxiaoteng <chengdaguo127@163.com>
2023-12-14 11:41:36 +0800

    Update data 12.01-07.

--- generate_result_py/generate_line_sum_csv.py
+++ generate_result_py/generate_line_sum_csv.py
@@ -9,7 +9,7 @@ def generate_line_sum_csv(path_name):
     df_router["aliased prefix"] = df_router["aliased prefix"] + df_seed["aliased prefix"]
     df_router["all prefix"] = df_router["all prefix"]
     df_router["sat aliased prefix"] = df_router["sat aliased prefix"] + df_seed["sat aliased prefix"]
-    df_router["sat all prefix"] = df_router["sat all prefix"] + df_seed["sat all prefix"]
+    df_router["sat all prefix"] = df_router["sat all prefix"]
     df_all = pd.DataFrame(df_router)
     # df_all.columns = ["aliased prefix", "all prefix", "sat aliased prefix", "sat all prefix"]
     df_all.to_csv(path_name+"sum_prefix_sat_change.csv", index=False)
